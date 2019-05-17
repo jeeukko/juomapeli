c7ece0d70a68f414b43e1e1b155d18fe29d97240
Juho Häkkinen <juho@juhohakkinen.fi>
2019-05-17 22:12:45 +0300

    Big rework

--- js/juomapeli.js
+++ js/juomapeli.js
@@ -1,15 +1,50 @@
+var deck = [];
+
 function newCard() {
-	var arvot = ["ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "jack", "queen", "king"];
-	var maat = ["clubs", "diamonds", "hearts", "spades"];
-
-	var arvo = Math.floor(Math.random() * (arvot.length + 1));
-	var maa = Math.floor(Math.random() * maat.length);
-
-	var pee = document.getElementById("kortti-img");
-
-	if (arvo == 13) {
-		pee.src = "img/joker.png";
-	} else {
-		pee.src = "img/" + arvot[arvo] + "_of_" + maat[maa] + ".png";
+	if (deck.length == 0) {
+		var suits = [
+	        "clubs",
+	        "diamonds",
+	        "hearts",
+	        "spades"
+	    ];
+	    var ranks = [
+	        "ace",
+	        "2",
+	        "3",
+	        "4",
+	        "5",
+	        "6",
+	        "7",
+	        "8",
+	        "9",
+	        "10",
+	        "jack",
+	        "queen",
+	        "king"
+	    ];
+	    
+	    for (var s = 0; s < suits.length; s++) {
+	        for (var r = 0; r < ranks.length; r++) {
+	            deck.push([
+	                ranks[r],
+	                suits[s]
+	            ]);
+	        }
+	    }
+		
+		var counter = deck.length;
+	    while (counter > 0) {
+	        var index = Math.floor(Math.random() * counter);
+	        counter--;
+	        var temp = deck[counter];
+	        deck[counter] = deck[index];
+	        deck[index] = temp;
+	    }
+		
+		console.log(deck);
 	}
+	
+	var card = deck.shift();
+	$("#card-img").attr("src", "img/cards/" + card[0] + "_of_" + card[1] + ".png");
 };
